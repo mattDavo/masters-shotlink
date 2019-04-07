d5881f3793b0a57bdda62685cc86244b372e752e
Matthew Davidson <mad1512@optusnet.com.au>
2019-04-07 18:07:24 +1000

    Created a shots schema

---
+++ schema.sql
@@ -0,0 +1,16 @@
+create table shots(
+    player_id integer,
+    player text,
+    shot integer,
+    round_n integer,
+    par integer,
+    hole integer,
+    shot_type text,
+    distance_before text,
+    distance_after text,
+    shot_length text,
+    category_before text,
+    category_after text,
+    x integer,
+    y integer
+)
